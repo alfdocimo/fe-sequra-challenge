080d95cc6f38ee78c6e9721a594919da9b7aadfd
Alfredo Narvaez <alfredonado@gmail.com>
2025-03-11 16:59:06 +0100

    feat: init main widget display

--- pay-in-instalments-widget/src/widget/index.tsx
+++ pay-in-instalments-widget/src/widget/index.tsx
@@ -1,8 +1,18 @@
+import { widgetClassnamePrefix } from "@/shared/utils/widget-classname-prefix";
+import "./index.css";
 import { SelectInstalmentsOptions } from "@/widget/select-instalments-options/index";
+
+const baseClassName = widgetClassnamePrefix("widget-container");
 
 export const Widget = () => {
   return (
-    <div>
+    <div className={baseClassName}>
+      <div className={`${baseClassName}__header-info`}>
+        <span>Pagalo en</span>
+        <button className={`${baseClassName}__header-info__more-info-btn`}>
+          Mas info
+        </button>
+      </div>
       <SelectInstalmentsOptions />
     </div>
   );
