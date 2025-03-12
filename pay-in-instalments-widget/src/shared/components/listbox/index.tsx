904977c32dd5d48188c33ffb0fe1c03b98d9723d
Alfredo Narvaez <alfredonado@gmail.com>
2025-03-12 13:52:55 +0100

    feat: update chevron icons

--- pay-in-instalments-widget/src/shared/components/listbox/index.tsx
+++ pay-in-instalments-widget/src/shared/components/listbox/index.tsx
@@ -127,7 +127,9 @@ function ToggleButton() {
         setIsOpen(!isOpen);
       }}
     >
-      <span>{isOpen ? "▲" : "▼"}</span>
+      <span>
+        {isOpen ? <ListBox.ChevronUpIcon /> : <ListBox.ChevronDownIcon />}
+      </span>
     </button>
   );
 }
@@ -162,7 +164,37 @@ function Item<ListBoxType>({
   );
 }
 
+const ChevronUpIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+  >
+    <path d="m18 15-6-6-6 6" />
+  </svg>
+);
+
+const ChevronDownIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+  >
+    <path d="m6 9 6 6 6-6" />
+  </svg>
+);
+
 ListBox.Item = Item;
 ListBox.Content = Content;
 ListBox.SelectedItem = SelectedItem;
 ListBox.ToggleButton = ToggleButton;
+ListBox.ChevronDownIcon = ChevronDownIcon;
+ListBox.ChevronUpIcon = ChevronUpIcon;
