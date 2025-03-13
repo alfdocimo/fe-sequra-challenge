787dd9fd9f8ff588080607de5b0bef70b025963f
Alfredo Narvaez <alfredonado@gmail.com>
2025-03-13 15:48:24 +0100

    refactor: remove unused code

--- pay-in-instalments-widget/vite.config.ts
+++ pay-in-instalments-widget/vite.config.ts
@@ -3,7 +3,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import { configDefaults } from "vitest/config.js";
 
 export default defineConfig({
   plugins: [react()],
